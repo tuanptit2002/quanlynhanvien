c456f628196dcfed90338749b08f46f8f2488942
tuanptit2002 <tuantienti952002@gmail.com>
2024-08-30 22:19:31 +0700

    fix:login
    Signed-off-by: tuanptit2002 <tuantienti952002@gmail.com>

--- src/main/java/com/example/QuanLyNhanVien/Repository/UserRepository.java
+++ src/main/java/com/example/QuanLyNhanVien/Repository/UserRepository.java
@@ -10,6 +10,8 @@ public interface UserRepository extends JpaRepository<User, String> {
     @Query(value = "select * from user u where u.ma_nv = :manv", nativeQuery = true)
     User findByMaNV(String manv);
 
+
+    @Query(value = "select * from user u where u.username = :username or u.ma_nv = :username", nativeQuery = true)
     User findByUsername(String username);
 
 //    @Query(value = "select * from user u where u.ma_nv = :manv", nativeQuery = true)
